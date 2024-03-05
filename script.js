b000c59a552ab63980b41de108a75b12e9d0d280
FShibukawa <felipeshibukawa@gmail.com>
2024-03-05 04:25:10 -0300

    add arithmetic operations

--- script.js
+++ script.js
@@ -1,15 +1,207 @@
+const buttonList = ["AC","<<","%","÷","7","8","9","x","4","5","6","-","1","2","3","+","0",".","+/-","="]
+const elementList = [{tag: 'div', id: 'previousDisplay', content: '50 + 658'},
+{ tag: 'div', id: 'display', content: '0'}];
+
+createAndAppendNodes(elementList);
+createAndAppendButton(buttonList);
+
 const buttons = document.querySelectorAll('button');
 
+let firstNumber = null;
+let operator;
+let lastNumber = null;
+let oldLastNumber;
+let accumulator = null;
+let oldAcummulator = null;
+let result;
+let equalClicked = false;
+let useAccumulator = false;
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const buttonId = button.id;
+        switch (buttonId) {
+            case 'AC':
+                console.log('CLEAR');
+                break;
+            case '=':
+                console.log('vc apertou ' + buttonId + "\noperador: " + operator)
+                operate();
+                equalClicked = true;
+                updateDisplay();
+                console.log('\nfirstNumber: ' + firstNumber +
+            ' \nlastNumber: ' + lastNumber + '\naccumulator: ' + accumulator + "\noldAcc: " + oldAcummulator)
+                break;
+            case '+':
+                console.log('vc apertou ' + buttonId)
+                operator = '+';
+                updateDisplay()
+                break;
+            case '-':
+                console.log('vc apertou ' + buttonId)
+                operator = '-';
+                updateDisplay()
+                break;
+            case 'x':
+                console.log('vc apertou ' + buttonId)
+                operator = '*';
+                updateDisplay()
+                break;
+            case '÷':
+                console.log('vc apertou ' + buttonId)
+                operator = '/';
+                updateDisplay()
+                break;
+            case '%':
+                operator = '%'
+                break;
+            case '<<':
+                console.log('ainda terá uma função')
+                break;
+            case '+/-':
+                
+                break;
+            case '.':
+
+                break;
+            default:
+                getNumber(buttonId);
+                updateDisplay()
+                console.log('default')
+        }
+
+    });
+});
+
+function getNumber(n) {
+    if (!firstNumber) {
+        firstNumber = n;
+    } else if (firstNumber && !operator) {
+        firstNumber = firstNumber + n;
+    } else if (operator && !lastNumber) {
+        lastNumber = n;
+    } else if (operator && lastNumber) {
+        lastNumber = lastNumber + n;
+    }
+}
+
+function operate() {
+    let parsedFirstNumber = parseInt(firstNumber);
+    let parsedlastNumber = parseInt(lastNumber);
+    oldAcummulator = accumulator;
+
+    if (accumulator) {
+        switch (operator) {
+            case '+':
+                result = add(accumulator, parsedlastNumber);
+                accumulator = result;
+                console.log('voce acabou de operar um ' + operator);
+                break;
+            case '-':
+                result = subtract(accumulator, parsedlastNumber);
+                accumulator = result;
+                break;
+            case '*':
+                result = multiply(accumulator, parsedlastNumber);
+                accumulator = result;
+                break;
+            case '/':
+                result = divide(accumulator, parsedlastNumber);
+                accumulator = result;
+                break;
+            case '%':
+                result = percent(accumulator, parsedlastNumber);
+                accumulator = result;
+                break;
+            default:
+                console.log('default do operate c acc');
+        }
+    }
+
+    if (!accumulator) {
+        switch (operator) {
+            case '+':
+                result = add(parsedFirstNumber, parsedlastNumber);
+                accumulator = result;
+                console.log('voce acabou de operar um ' + operator);
+                break;
+            case '-':
+                result = subtract(parsedFirstNumber, parsedlastNumber);
+                accumulator = result;
+                break;
+            case '*':
+                result = multiply(parsedFirstNumber, parsedlastNumber);
+                accumulator = result;
+                break;
+            case '/':
+                result = divide(parsedFirstNumber, parsedlastNumber);
+                accumulator = result;
+                break;
+            case '%':
+                result = percent(parsedFirstNumber, parsedlastNumber);
+                accumulator = result;
+                break;
+            default:
+                console.log('default do operate');
+        }
+    }
+    oldLastNumber = lastNumber;
+    lastNumber = null;
+
+}
+
+function updateDisplay() {
+    const display = document.getElementById("display");
+    const previousDisplay = document.getElementById("previousDisplay");
+
+    if (!operator) {
+        display.textContent = firstNumber || '0';
+        previousDisplay.textContent = ''; // Limpa o previousDisplay
+    } else if (!lastNumber && !oldLastNumber) {
+        display.textContent = firstNumber + " " + operator;
+    } else if (!accumulator && !oldLastNumber) { 
+        display.textContent = firstNumber + " " + operator + " " + lastNumber;
+        console.log(3)
+    } else if (equalClicked === true && !useAccumulator) {
+        display.textContent = result;
+        previousDisplay.textContent = firstNumber + " " + operator + " " + oldLastNumber + " =";
+        useAccumulator = true;
+        console.log(4)
+    } else {
+        display.textContent = accumulator + (lastNumber ? " " + operator + " " + lastNumber : '');
+        previousDisplay.textContent = (oldAcummulator ? oldAcummulator : firstNumber) + " " + operator + " " + oldLastNumber + " =";
+        console.log(5)
+    }
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function percent(a, b) {
+    return a * b * 0.01;
+}
+
+// UI functions
+
 function createNode(options) {
-    const { tag, classNames, content, htmlContent } = options;
+    const { tag, id, content, htmlContent } = options;
     const element = document.createElement(tag);
-  
-    if (classNames) {
-      if (Array.isArray(classNames)) {
-        classNames.forEach(className => element.classList.add(className));
-      } else {
-        element.classList.add(classNames);
-      }
+
+    if (id) {
+        element.id = id;
     }
   
     if (htmlContent) {
@@ -54,16 +246,3 @@ function createAndAppendButton(buttonList) {
         document.getElementById('calculator').appendChild(newButton)
     })
 }
-
-const buttonList = ["AC","<<","%","÷","7","8","9","X","4","5","6","-","1","2","3","+","0",".","+/-","="]
-  
-const elementList = [
-{
-    tag: 'div',
-    classNames: 'display',
-    content: '0'
-}
-];
-
-createAndAppendNodes(elementList);
-createAndAppendButton(buttonList)
